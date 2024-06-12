359be57a7337d7bc15c94373d1abf5141a2800b6
OkunElya <okun.elya@gmail.com>
2024-06-12 13:19:34 +1000

    added more music and ability to view spectrum, updated README.md, minor visual changes

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const overlay = document.getElementById('overlay');
     const pulseImage = document.getElementById('pulseImage');
     const visualizer = document.getElementById('visualizer');
-
-    fetch('music.mp3')
+    
+    songNum=Math.floor(Math.random() * 3);
+    fetch('music'+songNum+'.mp3')
         .then(response => {
+            
             const contentLength = response.headers.get('content-length');
             if (!contentLength) {
                 throw new Error('Content-Length response header unavailable');
@@ -52,19 +54,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
             source.connect(analyser);
             analyser.connect(audioContext.destination);
-
             visualizer.addEventListener('click', function() {
                 overlay.style.display = 'none';
                 document.getElementById("container").classList.add("spin");
                 source.start(0);
 
+                // const canvas = document.getElementById("spectrum"); //canvas for visual debug
+                // const ctx = canvas.getContext("2d");
+                
                 function animate() {
                     requestAnimationFrame(animate);
 
                     analyser.getByteFrequencyData(dataArray);
-                    const bass = dataArray.slice(0, bufferLength / 4).reduce((a, b) => a + b, 0);
 
-                    const scale = (1 + bass) / 1000;
+                    const bass = dataArray.slice(bufferLength / 12, bufferLength / 6).reduce((a, b) => a + b, 0)/((bufferLength/12)*256);//now variates between 1 and 0
+                    const mids = dataArray.slice((bufferLength / 12)*4, (bufferLength / 12)*5).reduce((a, b) => a + b, 0)/((bufferLength/12)*256);
+                    const highs = dataArray.slice((bufferLength / 12)*9, (bufferLength / 12)*10).reduce((a, b) => a + b, 0)/((bufferLength/12)*256);
+                    
+                    // ctx.fillStyle = "white";
+                    // ctx.fillRect(0, 0, 256, 256);
+                    // ctx.moveTo(0,256);
+                    // ctx.beginPath();
+                    // ctx.strokeStyle="green";
+                    // for(i=0;i<128;i++){
+                    //     ctx.lineTo(i*2,256-dataArray[i]);
+                    // }
+                    // ctx.stroke();
+
+                    // ctx.beginPath();
+                    // ctx.strokeStyle="red";
+                    // ctx.moveTo(0,256-(bass*256));
+                    // ctx.lineTo(256,256-(bass*256));
+                    // ctx.closePath();
+                    // ctx.stroke();
+
+                    // ctx.beginPath();
+                    // ctx.strokeStyle="yellow";
+                    // ctx.moveTo(0,256-(mids*256));
+                    // ctx.lineTo(256,256-(mids*256));
+                    // ctx.closePath();
+                    // ctx.stroke();
+
+                    // ctx.beginPath();
+                    // ctx.strokeStyle="blue";
+                    // ctx.moveTo(0,256-(highs*256));
+                    // ctx.lineTo(256,256-(highs*256));
+                    // ctx.closePath();
+                    // ctx.stroke();
+
+                    const scale = (3 + bass*2 +mids + highs);
                     pulseImage.style.transform = `scale(${scale})`;
                 }
 
